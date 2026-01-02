c473ab1cc2e33088affb2530a2fb4e0ac4df0a37
edupavlov <edupavlov@ozon.ru>
2026-01-02 12:36:35 +0300

    Update nuxt.config for correct deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	ssr: true,
-  compatibilityDate: '2025-07-15',
+	compatibilityDate: '2025-07-15',
 	modules: ['@nuxt/ui'],
 	css: ['~/assets/css/main.css'],
-  devtools: { enabled: true },
+	devtools: { enabled: process.env.NODE_ENV !== 'production' },
+  app: {
+    baseURL: process.env.NUXT_APP_BASE_URL || '/',
+    buildAssetsDir: '/_nuxt/'
+  },
+  nitro: {
+    preset: process.env.NITRO_PRESET || '',
+    prerender: {
+      crawlLinks: true
+    }
+  },
 })
